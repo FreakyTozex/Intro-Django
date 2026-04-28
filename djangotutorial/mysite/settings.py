1159b075f995170f50ee534ca5b07f8e7d04ac6b
FreakyTozex <antoniodepinho0611@gmail.com>
2026-04-28 10:18:46 +0100

    Adicionar .env e modificar .gitignore e .dockerignore

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -11,16 +11,25 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, "django-insecure-#we&9*ubl&@ol5gr57#xo*t6^9w2-=6hmu9y)lgst^2n!x$x=p"),
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#we&9*ubl&@ol5gr57#xo*t6^9w2-=6hmu9y)lgst^2n!x$x=p'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
